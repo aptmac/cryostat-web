15b4a1ed497fc8d6beeb5d2bcce45e0829bc3090
Alex Macdonald <almacdon@redhat.com>
2024-07-05 16:28:32 -0400

    Add simple iframe to render the dev:preview running in another process on localhost:9091

---
+++ src/app/Console/IFramePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export const IFramePage = () => {
+  return (
+    <iframe src="http://localhost:9091" style = {{height: "100%"}}/>
+  );
+};
+
+export default IFramePage;
